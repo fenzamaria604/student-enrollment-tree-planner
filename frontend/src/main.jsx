bf3e8eb315f80a6b725cf65c53b72fcc126264b1
fenzamaria604 <fenzawebyfy@gmail.com>
2026-06-04 16:17:48 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -20,4 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
->>>>>>> f7859f04762c55936115883b8d4be06eedf41722
